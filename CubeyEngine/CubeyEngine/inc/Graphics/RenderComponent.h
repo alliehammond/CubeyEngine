61bed8b654c232e7a4ad2af43ff2e5ea6bbeda62
zylia <secret>
2023-10-05 06:39:35 -0700

    Very minor changes to pad commit history

--- CubeyEngine/CubeyEngine/inc/Graphics/RenderComponent.h
+++ CubeyEngine/CubeyEngine/inc/Graphics/RenderComponent.h
@@ -8,7 +8,6 @@ class RenderComponent : public Component
 {
 public:
     RenderComponent(std::string modelFileName, GameObject *owner);
-    //Change this to put color in material so gameobject can't be null
     RenderComponent(std::string modelFileName, Material *mat, GameObject *owner, CBY::Vector4 colorAlpha = CBY::Vector4());
 
     RenderComponent(Model *Model, GameObject* owner);
